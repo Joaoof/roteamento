631291e23bb538efa4fc067dff16d82dce5b4bdb
Joaoof <joaodeus400@gmail.com>
2024-06-15 12:12:26 -0300

    feat: roteamento angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PrimeiraPaginaComponent } from '../app/primeira-pagina/primeira-pagina.component'; // Adjust the path as needed
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'primeira-pagina', component: PrimeiraPaginaComponent },
+    { path: '', redirectTo: '/primeira-pagina', pathMatch: 'full' } // Default route
+];
